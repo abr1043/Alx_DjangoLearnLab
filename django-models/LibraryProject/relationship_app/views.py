0b2c30801419ff750a300bfdf2474da52b84e42c
OJO ABRAHAM <abrahamojo79@gmail.com>
2025-09-18 06:06:25 -0700

    Added function-based and class-based views with proper template paths in relationship_app

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from django.views.generic import DetailView
 from .models import Book, Library
 
 # Function-based view: List all books
 def list_books(request):
     books = Book.objects.all()
-    return render(request, "list_books.html", {"books": books})
+    return render(request, "relationship_app/list_books.html", {"books": books})
+
 
 # Class-based view: Library detail
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = "library_detail.html"
+    template_name = "relationship_app/library_detail.html"
     context_object_name = "library"

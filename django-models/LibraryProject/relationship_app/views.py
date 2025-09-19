85d3ad48cff4fa0943bc505729d75c5a1fdae79f
OJO ABRAHAM <abrahamojo79@gmail.com>
2025-09-19 05:03:51 -0700

    Added login, logout, and registration views using Django built-in auth

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.views.generic.detail import DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin

71b8ed697954c5093604a3c197eb56725ef6cef8
OJO ABRAHAM <abr1043_abrahamojo79@gmail.com>
2025-10-01 12:24:39 -0700

    Configure database settings with USER and PORT fields

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "django_blog_db",     
+        "USER": "postgres",           
+        "PASSWORD": "your_password",  
+        "HOST": "localhost",          
+        "PORT": "5432",               
     }
 }
 
